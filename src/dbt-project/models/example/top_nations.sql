0035030b13d0365e740d981836a532e4b92aff81
Kevin Yan <kevin.yan@jhg.com.au>
2022-07-06 11:52:30 +1000

    update top_nations

--- src/dbt-project/models/example/top_nations.sql
+++ src/dbt-project/models/example/top_nations.sql
@@ -10,8 +10,8 @@
 
 {{ config(materialized='table') }}
 
-select n_name, count(*) as active_buyers
+select n_name, n_comment, count(*) as active_buyers
 from nation n left join {{ ref('top_customers') }} c on n.n_nationkey = c.c_nationkey
-group by n_name
+group by n_name, n_comment
 order by active_buyers desc
 limit 3
